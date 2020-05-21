9412fac73019a70f7424ca56baf9b1b715e85b84
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-05-21 22:28:44 +0200

    something

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var _ = require('underscore');
 
 let result = _.contains([1, 2, 3], 2);
 console.log(result);
+
+var lion = require('simona-lion');
+var result1 = lion.add(34,56);
+console.log(result1);
